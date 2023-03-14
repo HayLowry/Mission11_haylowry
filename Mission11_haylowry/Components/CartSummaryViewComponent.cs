9464f9cd3db4f3406b794a271b1487de9d60c8ca
haylowry <haylow77@gmail.com>
2023-03-14 09:23:47 -0600

    added cart icon and cart summary component view

---
+++ Mission11_haylowry/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Mission11_haylowry.Models;
+
+namespace Mission11_haylowry.Components
+{
+    public class CartSummaryViewComponent : ViewComponent 
+    {
+        private Cart cart { get; set; }
+        public CartSummaryViewComponent (Cart cartService)
+        {
+            cart = cartService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+    }
+}
